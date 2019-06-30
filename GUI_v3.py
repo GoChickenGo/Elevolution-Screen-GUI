2982e00669970c5d9f325fd32737e2423e03d7b5
GoChickenGo <49279131+GoChickenGo@users.noreply.github.com>
2019-06-30 16:13:58 +0200

    Add files via upload

--- GUI_v3.py
+++ GUI_v3.py
@@ -11,7 +11,9 @@ from PyQt5.QtWidgets import QWidget, QLabel, QGridLayout, QPushButton, QVBoxLayo
 import numpy as np
 from StagescanClass import Stagescan
 from matplotlib import pyplot as plt
-import pyqtgraph as pg
+#import pyqtgraph as pg
+from IPython import get_ipython
+from matplotlib.ticker import FormatStrFormatter
 
 class setADsamples(QtWidgets.QDialog):
 
@@ -22,7 +24,7 @@ class setADsamples(QtWidgets.QDialog):
         #----------------------------------GUI---------------------------------
         #----------------------------------------------------------------------
         self.setMinimumSize(300,120)
-        self.setWindowTitle("A/D signals")
+        self.setWindowTitle("Digital signals")
         
         #self.waveContainer = QGroupBox("Signals")
         Layout = QGridLayout() #Layout manager
@@ -76,39 +78,39 @@ class setADsamples(QtWidgets.QDialog):
         Layout.addWidget(self.button_box, 1, 9)
         
         # 2nd digital line
-        self.textboxJ = QComboBox()
-        self.textboxJ.addItems(['1','0'])
-        Layout.addWidget(self.textboxJ, 2, 0)
+        self.textbox2A = QComboBox()
+        self.textbox2A.addItems(['0','1'])
+        Layout.addWidget(self.textbox2A, 2, 0)
         
-        self.textboxK = QPlainTextEdit(self)
-        Layout.addWidget(self.textboxK, 3, 1)
+        self.textbox2B = QPlainTextEdit(self)
+        Layout.addWidget(self.textbox2B, 3, 1)
         Layout.addWidget(QLabel("Frequency:"), 2, 1)
 
-        self.textboxL = QPlainTextEdit(self)
-        Layout.addWidget(self.textboxL, 3, 2)
+        self.textbox2C = QPlainTextEdit(self)
+        Layout.addWidget(self.textbox2C, 3, 2)
         Layout.addWidget(QLabel("Offset (ms):"), 2, 2)
         
-        self.textboxM = QPlainTextEdit(self)
-        Layout.addWidget(self.textboxM, 3, 3)
+        self.textbox2D = QPlainTextEdit(self)
+        Layout.addWidget(self.textbox2D, 3, 3)
         Layout.addWidget(QLabel("Period (ms):"), 2, 3)   
         
-        self.textboxN = QPlainTextEdit(self)
-        Layout.addWidget(self.textboxN, 3, 4)
+        self.textbox2E = QPlainTextEdit(self)
+        Layout.addWidget(self.textbox2E, 3, 4)
         Layout.addWidget(QLabel("Repeat times:"), 2, 4) 
         
         Layout.addWidget(QLabel("DC (%):"), 2, 5)
-        self.textboxO = QComboBox()
-        self.textboxO.addItems(['50','10'])
-        Layout.addWidget(self.textboxO, 3, 5)
+        self.textbox2F = QComboBox()
+        self.textbox2F.addItems(['50','10'])
+        Layout.addWidget(self.textbox2F, 3, 5)
         
         Layout.addWidget(QLabel("Digital used line:"), 2, 6)
-        self.textboxP = QComboBox()
-        self.textboxP.addItems(['/Dev1/port0/line24','10'])
-        Layout.addWidget(self.textboxP, 3, 6)
+        self.textbox2G = QComboBox()
+        self.textbox2G.addItems(['/Dev1/port0/line24','10'])
+        Layout.addWidget(self.textbox2G, 3, 6)
         
         # 3rd digital line
         self.textbox3A = QComboBox()
-        self.textbox3A.addItems(['1','0'])
+        self.textbox3A.addItems(['0','1'])
         Layout.addWidget(self.textbox3A, 4, 0)
         
         self.textbox3B = QPlainTextEdit(self)
@@ -139,7 +141,7 @@ class setADsamples(QtWidgets.QDialog):
         
         # 4th digital line
         self.textbox4A = QComboBox()
-        self.textbox4A.addItems(['1','0'])
+        self.textbox4A.addItems(['0','1'])
         Layout.addWidget(self.textbox4A, 6, 0)
         
         self.textbox4B = QPlainTextEdit(self)
@@ -187,7 +189,7 @@ class setADsamples(QtWidgets.QDialog):
         
     def on_click1(self):
         
-        # generate wave
+        # generate wave 1, by default the master one--------------------------11111111111111--------------------
         self.samplingrate_1 = int(self.textboxI.currentText()) # samples per second
         
         #textboxValue1 = self.textboxA.currentText()
@@ -219,8 +221,185 @@ class setADsamples(QtWidgets.QDialog):
             
         self.final_wave_1 = np.append(self.offsetsamples_1, self.wave_1)
         
+        lengthofmasterwave = len(self.final_wave_1)
+        
+        # ---------------------------------generate wave 2-------------------22222222222222-------------
+        
+        if int(self.textbox2A.currentText()) == 1:
+            self.samplingrate_2 = int(self.textboxI.currentText()) # samples per second
+        
+            #textboxValue1 = self.textboxA.currentText()
+            
+            self.wavefrequency_2 = int(self.textbox2B.toPlainText())
+            self.waveoffset_2 = int(self.textbox2C.toPlainText()) # in ms
+            self.waveperiod_2 = int(self.textbox2D.toPlainText())
+            self.waveDC_2 = int(self.textbox2F.currentText())
+            self.waverepeat_2 = int(self.textbox2E.toPlainText())
+            
+            #self.wave = np.array([textboxValue1])
+            
+            self.offsetsamples_number_2 = int(1 + (self.waveoffset_2/1000)*self.samplingrate_2) # By default one 0 is added so that we have a rising edge at the beginning.
+            self.offsetsamples_2 = np.zeros(self.offsetsamples_number_2, dtype=bool) # Be default offsetsamples_number is an integer.
+            
+            self.sample_num_singleperiod_2 = round((int((self.waveperiod_2/1000)*self.samplingrate_2))/self.wavefrequency_2)
+            self.true_sample_num_singleperiod_2 = round((self.waveDC_2/100)*self.sample_num_singleperiod_2)
+            self.false_sample_num_singleperiod_2 = self.sample_num_singleperiod_2 - self.true_sample_num_singleperiod_2
+            
+            self.sample_singleperiod_2 = np.append(np.ones(self.true_sample_num_singleperiod_2,dtype=bool), np.zeros(self.false_sample_num_singleperiod_2,dtype=bool))
+            
+            self.repeatnumberintotal_2 = self.wavefrequency_2*(self.waveperiod_2/1000)*self.waverepeat_2
+            
+            if self.repeatnumberintotal_2 >= 1:
+                self.wave_2 = np.tile(self.sample_singleperiod_2, int(self.repeatnumberintotal_2))
+            else:
+                self.partial_numb_2 = int(self.repeatnumberintotal_2*self.sample_num_singleperiod_2)
+                self.wave_2 = self.sample_singleperiod_2[0:self.partial_numb_2]
+                
+            self.final_wave_2 = np.append(self.offsetsamples_2, self.wave_2)
+            
+            # add or cut wave 2 according to length of master wave 1
+            if len(self.final_wave_2) >= lengthofmasterwave:
+                
+                self.final_wave_2 = self.final_wave_2[0:lengthofmasterwave]
+            else:
+                self.offsetsamples_master_2 = np.zeros((lengthofmasterwave-len(self.final_wave_2)), dtype=bool)
+                self.final_wave_2 = np.append(self.final_wave_2, self.offsetsamples_master_2)
+        else:
+            self.final_wave_2 = []
+            
+        #print (self.final_wave_2)
+            
+        #-------------------------------GENERATE WAVE 3333333333333333333333333----------------------------------
+        
+        if int(self.textbox3A.currentText()) == 1:
+            self.samplingrate_3 = int(self.textboxI.currentText()) # samples per second
+        
+            #textboxValue1 = self.textboxA.currentText()
+            
+            self.wavefrequency_3 = int(self.textbox3B.toPlainText())
+            self.waveoffset_3 = int(self.textbox3C.toPlainText()) # in ms
+            self.waveperiod_3 = int(self.textbox3D.toPlainText())
+            self.waveDC_3 = int(self.textbox3F.currentText())
+            self.waverepeat_3 = int(self.textbox3E.toPlainText())
+            
+            #self.wave = np.array([textboxValue1])
+            
+            self.offsetsamples_number_3 = int(1 + (self.waveoffset_3/1000)*self.samplingrate_3) # By default one 0 is added so that we have a rising edge at the beginning.
+            self.offsetsamples_3 = np.zeros(self.offsetsamples_number_3, dtype=bool) # Be default offsetsamples_number is an integer.
+            
+            self.sample_num_singleperiod_3 = round((int((self.waveperiod_3/1000)*self.samplingrate_3))/self.wavefrequency_3)
+            self.true_sample_num_singleperiod_3 = round((self.waveDC_3/100)*self.sample_num_singleperiod_3)
+            self.false_sample_num_singleperiod_3 = self.sample_num_singleperiod_3 - self.true_sample_num_singleperiod_3
+            
+            self.sample_singleperiod_3 = np.append(np.ones(self.true_sample_num_singleperiod_3,dtype=bool), np.zeros(self.false_sample_num_singleperiod_3,dtype=bool))
+            
+            self.repeatnumberintotal_3 = self.wavefrequency_3*(self.waveperiod_3/1000)*self.waverepeat_3
+            
+            if self.repeatnumberintotal_3 >= 1:
+                self.wave_3 = np.tile(self.sample_singleperiod_2, int(self.repeatnumberintotal_3))
+            else:
+                self.partial_numb_3 = int(self.repeatnumberintotal_3*self.sample_num_singleperiod_3)
+                self.wave_3 = self.sample_singleperiod_3[0:self.partial_numb_3]
+                
+            self.final_wave_3 = np.append(self.offsetsamples_3, self.wave_3)
+            
+            # add or cut wave 2 according to length of master wave 1
+            if len(self.final_wave_3) >= lengthofmasterwave:
+                
+                self.final_wave_3 = self.final_wave_3[0:lengthofmasterwave]
+            else:
+                self.offsetsamples_master_3 = np.zeros((lengthofmasterwave-len(self.final_wave_3)), dtype=bool)
+                self.final_wave_3 = np.append(self.final_wave_3, self.offsetsamples_master_3)
+        else:
+            self.final_wave_3 = []        
+        
+        ##-------------------------------GENERATE WAVE 44444444444444444444444444444----------------------------------
+        
+        if int(self.textbox4A.currentText()) == 1:
+            self.samplingrate_4 = int(self.textboxI.currentText()) # samples per second
+        
+            #textboxValue1 = self.textboxA.currentText()
+            
+            self.wavefrequency_4 = int(self.textbox4B.toPlainText())
+            self.waveoffset_4 = int(self.textbox4C.toPlainText()) # in ms
+            self.waveperiod_4 = int(self.textbox4D.toPlainText())
+            self.waveDC_4 = int(self.textbox4F.currentText())
+            self.waverepeat_4 = int(self.textbox4E.toPlainText())
+            
+            #self.wave = np.array([textboxValue1])
+            
+            self.offsetsamples_number_4 = int(1 + (self.waveoffset_4/1000)*self.samplingrate_4) # By default one 0 is added so that we have a rising edge at the beginning.
+            self.offsetsamples_4 = np.zeros(self.offsetsamples_number_4, dtype=bool) # Be default offsetsamples_number is an integer.
+            
+            self.sample_num_singleperiod_4 = round((int((self.waveperiod_4/1000)*self.samplingrate_4))/self.wavefrequency_4)
+            self.true_sample_num_singleperiod_4 = round((self.waveDC_4/100)*self.sample_num_singleperiod_4)
+            self.false_sample_num_singleperiod_4 = self.sample_num_singleperiod_4 - self.true_sample_num_singleperiod_4
+            
+            self.sample_singleperiod_4 = np.append(np.ones(self.true_sample_num_singleperiod_4,dtype=bool), np.zeros(self.false_sample_num_singleperiod_4,dtype=bool))
+            
+            self.repeatnumberintotal_4 = self.wavefrequency_4*(self.waveperiod_4/1000)*self.waverepeat_4
+            
+            if self.repeatnumberintotal_4 >= 1:
+                self.wave_4 = np.tile(self.sample_singleperiod_2, int(self.repeatnumberintotal_4))
+            else:
+                self.partial_numb_4 = int(self.repeatnumberintotal_4*self.sample_num_singleperiod_4)
+                self.wave_4 = self.sample_singleperiod_4[0:self.partial_numb_4]
+                
+            self.final_wave_4 = np.append(self.offsetsamples_4, self.wave_4)
+            
+            # add or cut wave 2 according to length of master wave 1
+            if len(self.final_wave_4) >= lengthofmasterwave:
+                
+                self.final_wave_4 = self.final_wave_4[0:lengthofmasterwave]
+            else:
+                self.offsetsamples_master_4 = np.zeros((lengthofmasterwave-len(self.final_wave_4)), dtype=bool)
+                self.final_wave_4 = np.append(self.final_wave_4, self.offsetsamples_master_4)
+        else:
+            self.final_wave_4 = []         
+        
         #self.my_plot.plot(range(len(self.final_wave_1)), self.final_wave_1)
-        pg.plot(range(len(self.final_wave_1)), self.final_wave_1, pen=None, symbol='o')
+        #pg.plot(range(len(self.final_wave_1)), self.final_wave_1, pen=None, symbol='o')
+        xlabel = np.arange(lengthofmasterwave)/self.samplingrate_1
+        dirforwave = {}
+        
+        number_of_Dchannels = int(self.textboxA.currentText())+int(self.textbox2A.currentText())+int(self.textbox3A.currentText())+int(self.textbox4A.currentText())
+        
+        dirforwave[0] = np.array([self.final_wave_1, self.textboxF.currentText()])
+        if len(self.final_wave_2) != 0:
+            dirforwave[1] = np.array([self.final_wave_2, self.textbox2G.currentText()])
+        else:
+            dirforwave[1] = []
+        if len(self.final_wave_3) != 0:
+            dirforwave[2] = np.array([self.final_wave_3, self.textbox3G.currentText()])
+        else:
+            dirforwave[2] = []
+        if len(self.final_wave_4) != 0:
+            dirforwave[3] = np.array([self.final_wave_4, self.textbox4G.currentText()])
+        else:
+            dirforwave[3] = []
+        
+        get_ipython().run_line_magic('matplotlib', 'qt') # set spyder to seperate window when matplot display image.
+        plt.figure()
+        plt.plot(xlabel, self.final_wave_1)
+        if len(self.final_wave_2) != 0:
+            plt.plot(xlabel, self.final_wave_2)
+        if len(self.final_wave_3) != 0:
+            plt.plot(xlabel, self.final_wave_3)
+        if len(self.final_wave_4) != 0:
+            plt.plot(xlabel, self.final_wave_4)            
+
+        plt.gca().xaxis.set_major_formatter(FormatStrFormatter('%d s'))
+        plt.show()
+
+        #self.Digital_container = np.vstack((dirforwave[0:number_of_Dchannels]))
+        '''
+        dtype_wave = [('waveform array', object), ('DAQ line', 'S30')]
+        self.waveholder = np.zeros((1+len(self.final_wave_2)+len(self.final_wave_3)+len(self.final_wave_4)), dtype = dtype_wave)
+        
+        for p in range(1+len(self.final_wave_2)+len(self.final_wave_3)+len(self.final_wave_4)):
+            self.waveholder[p] = dirforwave[p]
+        print(self.waveholder['DAQ line'])
+        '''
         
 class MyWindow(QtWidgets.QWidget):
 
@@ -299,7 +478,7 @@ class MyWindow(QtWidgets.QWidget):
         Layout.addWidget(QLabel("average over:"), 6, 0)
 
         # Create a button in the window
-        self.button1 = QPushButton('Feed AD signals', self)
+        self.button1 = QPushButton('Feed Digital signals', self)
         Layout.addWidget(self.button1, 8, 0)
         
         self.button2 = QPushButton('Start', self)
@@ -359,8 +538,11 @@ class MyWindow(QtWidgets.QWidget):
         textboxValue13 = self.textboxM.currentText()
         UI_averagenum = int(textboxValue13)
         
+        #------------------------------------before start, set Spyder graphic back to inline-------------------------------
+        get_ipython().run_line_magic('matplotlib', 'inline') # before start, set spyder back to inline
+        
         digitalwave1 = self.wave1 # digital wave feed to start()
-        plt.plot(digitalwave1)
+        plt.plot(digitalwave1[0][0])
         plt.show()
         
         #self.S = Stagescan(UI_row_start, UI_row_end, UI_column_start, UI_column_end, UI_step, UI_Daq_sample_rate, UI_voltXMin, UI_voltXMax, UI_voltYMin, UI_voltYMax, UI_Value_xPixels, UI_Value_yPixels, UI_averagenum)
@@ -370,9 +552,9 @@ class MyWindow(QtWidgets.QWidget):
     def get_login(self):
         login = setADsamples()
         if login.exec_():
-            self.wave1 = login.final_wave_1
+            self.wave1 = login.Digital_container
             #self.edit.setText(login.password.text())
-            plt.plot(self.wave1)
+            plt.plot(self.wave1[:][0])
             plt.show()
 
 
